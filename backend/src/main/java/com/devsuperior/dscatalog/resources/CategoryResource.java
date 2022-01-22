dbcf2452c4cbff59e0c8568d88d69c31a01c8184
Michael Zafanelli <maicomdassi@gmail.com>
2022-01-21 22:30:25 -0300

    DataBase Seeding

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -20,7 +20,7 @@ public class CategoryResource {
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+		List<Category> list = service.findAll(); 
 		return ResponseEntity.ok().body(list);
 	}
 
