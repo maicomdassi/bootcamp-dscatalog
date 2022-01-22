f0c609744a080f349602add235bdce7bdc370e84
Michael Zafanelli <maicomdassi@gmail.com>
2022-01-22 16:07:51 -0300

    Auditing data

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController
@@ -49,5 +51,13 @@ public class CategoryResource {
 		dto = service.update(id,dto);
 		
 		return ResponseEntity.ok().body(dto);
+	} 
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id) {
+		
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
 	} 
 }
