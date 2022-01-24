47a2bc668316b887d34f0b58fbdd14d61b96b005
Michael Zafanelli <maicomdassi@gmail.com>
2022-01-24 19:01:06 -0300

    Routs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import Home from './pages/Home';
+
 import './assets/styles/custom.scss';
 import './App.css';
+import Routes from 'Routes';
 
-const App = () => {
+function App(){
   return (   
-      <Home />    
+     <Routes/> 
   );
 }
 
