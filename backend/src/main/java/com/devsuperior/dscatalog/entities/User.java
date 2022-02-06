b1616e41444b70ef41e63b34c6af627b86ad3254
Michael Zafanelli <maicomdassi@gmail.com>
2022-02-06 18:17:46 -0300

    H2 security configuration

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -22,10 +22,9 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
 @Entity
 @Table(name = "tb_user")
-public class User implements UserDetails, Serializable{
+public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -36,14 +35,13 @@ public class User implements UserDetails, Serializable{
 	@Column(unique = true)
 	private String email;
 	private String password;
-	
+
 	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "tb_user_role", 
-	joinColumns = @JoinColumn(name = "user_id"), 
-	inverseJoinColumns = @JoinColumn(name = "role_id"))	
+	@JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();
-	
-	public User () {}
+
+	public User() {
+	}
 
 	public User(Long id, String firstName, String lastName, String email, String password) {
 		super();
@@ -93,8 +91,6 @@ public class User implements UserDetails, Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-	
 
 	public Set<Role> getRoles() {
 		return roles;
@@ -124,7 +120,7 @@ public class User implements UserDetails, Serializable{
 
 	@Override
 	public String getUsername() {
-				return email;
+		return email;
 	}
 
 	@Override
@@ -150,6 +146,5 @@ public class User implements UserDetails, Serializable{
 		// TODO Auto-generated method stub
 		return true;
 	}
-	
-	
+
 }
