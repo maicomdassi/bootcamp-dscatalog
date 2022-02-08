5a91ce7497163e559b768d4c3a89e8cf721b9ec0
Michael Zafanelli <maicomdassi@gmail.com>
2022-02-08 19:00:21 -0300

    Banner styling

--- frontend/src/pages/Admin/Auth/index.tsx
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -1,29 +1,31 @@
 import { ReactComponent as AuthImage } from 'assets/images/auth-image.svg';
 import { Route, Switch } from 'react-router-dom';
 
+import './styles.css';
+
 const Auth = () => {
   return (
     <div className="auth-container">
       <div className="auth-banner-container">
-
-          <h1>Divulgue seus produtos no DS Catalog</h1>
-          <p>Faça parte do nosso catálago de divulgação e aumente as vendas de seus produtos</p>
-          <AuthImage />        
-
-            <div className='auth-from-container'>
-                <Switch>
-                    <Route path="/admin/auth/login">
-                        <h1>Card de Login</h1>
-                    </Route>
-                    <Route path="/admin/auth/signup">
-                        <h1>Card de Signup</h1>
-                    </Route>
-                    <Route path="/admin/auth/recover">
-                        <h1>Card de Recover</h1>
-                    </Route>
-                </Switch>
-
-            </div>
+        <h1>Divulgue seus produtos no DS Catalog</h1>
+        <p>
+          Faça parte do nosso catálago de divulgação e aumente as vendas de seus
+          produtos
+        </p>
+        <AuthImage />
+      </div>
+      <div className="auth-from-container">
+        <Switch>
+          <Route path="/admin/auth/login">
+            <h1>Card de Login</h1>
+          </Route>
+          <Route path="/admin/auth/signup">
+            <h1>Card de Signup</h1>
+          </Route>
+          <Route path="/admin/auth/recover">
+            <h1>Card de Recover</h1>
+          </Route>
+        </Switch>
       </div>
     </div>
   );
