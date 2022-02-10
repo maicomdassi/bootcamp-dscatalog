26330c45be3157cdb9cb5b99fcc79d8251e8504f
Michael Zafanelli <maicomdassi@gmail.com>
2022-02-10 14:51:56 -0300

    hasAnyHoles

--- frontend/src/util/requests.ts
+++ frontend/src/util/requests.ts
@@ -112,3 +112,24 @@ axios.interceptors.request.use(
 
     return (tokenData && tokenData.exp * 1000 > Date.now()) ? true : false;
   }
+
+  export const hashAnyRoles = (roles: Role[]) : boolean => {
+
+    if(roles.length === 0){ return true;}
+
+    const tokenData = getTokenData();
+
+//##########################################################################    
+    if(tokenData !== undefined){
+      return roles.some(role => tokenData.authorities.includes(role));
+    }
+/*     if(tokenData !== undefined){
+      for(var i = 0; i < roles.length; i++){
+        if(tokenData.authorities.includes(roles[i])){
+          return true
+        }
+      }
+    } */
+//########################################################################## 
+    return false;
+  }
